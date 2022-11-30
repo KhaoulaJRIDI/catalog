fb35209c12a2ca9ceab479d449bf206c82ab4322
Khaoula Jridi <khaoulajri@gmail.com>
2022-11-30 11:40:30 +0100

    committed 30-11-2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { NewProductComponent } from './new-product/new-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CustomersComponent,
+    NewProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/product.model.ts
@@ -0,0 +1,12 @@
+export interface Product{
+  id: string;
+  name : string;
+  price : number;
+  promotion: boolean;
+}
+export interface PageProduct{
+  products: Product[];
+  page: number;
+  size: number;
+  totalPages: number;
+}
